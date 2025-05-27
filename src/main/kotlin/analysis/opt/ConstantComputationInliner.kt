88581594ed3307b931ad959e35035e2a5351a3bf
John Toman <john@certora.com>
2025-05-27 17:09:18 +0300

    CERT-8971 Inline The Saturation
    9efe9ae2c782be8495419bd423451a2228fc065d

--- src/main/kotlin/analysis/opt/ConstantComputationInliner.kt
+++ src/main/kotlin/analysis/opt/ConstantComputationInliner.kt
@@ -167,7 +167,7 @@ object ConstantComputationInliner : ArrayLengthHeuristicMixin {
         } while(newSize > oldSize)
 
         fun locToReservedLocationScalar(base: TACSymbol, loc: BigInteger) : TACSymbol.Var? {
-            if(Config.EquivalenceCheck.get()) {
+            if(!Config.Mem0x0To0x40AsScalar) {
                 return null
             }
             if(base == TACKeyword.MEMORY.toVar() &&
